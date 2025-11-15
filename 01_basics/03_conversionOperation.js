1fe019abd4705fb18f43238fc96050fd73d8ff59
suv-script <suvrat202099@gmail.com>
2025-11-15 06:27:02 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33 abc"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+ let valueInNumber = Number(score)// permanently converts datatpe to number
+// console.log(typeof valueInNumber); // give type number
+// console.log(valueInNumber); //give NaN because score's value is not number
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true ; 0 => false
+// " " => false
+//"suvrat" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
